0573241676eaaf95e6d57696364d6fcc158f48a4
Akash Suryawanshi <suryaakash7799@gmail.com>
2020-11-13 13:49:20 +0530

    insert.

--- src/A1List.java
+++ src/A1List.java
@@ -3,50 +3,49 @@
 
 public class A1List extends List {
 
-    private A1List  next; // Next Node
+    private A1List next; // Next Node
     private A1List prev;  // Previous Node 
 
-    public A1List(int address, int size, int key) { 
+    public A1List(int address, int size, int key) {
         super(address, size, key);
     }
-    
-    public A1List(){
-        super(-1,-1,-1);
+
+    public A1List() {
+        super(-1, -1, -1);
         // This acts as a head Sentinel
 
-        A1List tailSentinel = new A1List(-1,-1,-1); // Intiate the tail sentinel
-        
+        A1List tailSentinel = new A1List(-1, -1, -1); // Intiate the tail sentinel
+
         this.next = tailSentinel;
         tailSentinel.prev = this;
     }
 
-    public A1List Insert(int address, int size, int key)
-    {
-        return null;
+    public A1List Insert(int address, int size, int key) {
+        A1List element = new A1List(address, size, key);
+        this.next.prev = element;
+        element.next = this.next;
+        this.next = element;
+        element.prev = this;
+        return element;
     }
 
-    public boolean Delete(Dictionary d) 
-    {
+    public boolean Delete(Dictionary d) {
         return false;
     }
 
-    public A1List Find(int k, boolean exact)
-    { 
+    public A1List Find(int k, boolean exact) {
         return null;
     }
 
-    public A1List getFirst()
-    {
-        return null;
-    }
-    
-    public A1List getNext() 
-    {
+    public A1List getFirst() {
         return null;
     }
 
-    public boolean sanity()
-    {
+    public A1List getNext() {
+        return null;
+    }
+
+    public boolean sanity() {
         return true;
     }
 
